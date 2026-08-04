fe33a8b1e05e1ffc27398b7f41f691a28ccb1f1b
Dolesh kumar <hanubhai0007@gmail.com>
2026-08-04 14:35:54 +0530

    Add files via upload

---
+++ program7.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class program7
+    {
+        static void Main(String[] args)
+        {
+            int n = 5;
+            int f = 1;
+            for (int i = 0; i < n; i++)
+            {
+                f = f * (n - 1);
+            }
+            Console.WriteLine("The factorial of the given number is: " + f);
+        }
+    }
+}
