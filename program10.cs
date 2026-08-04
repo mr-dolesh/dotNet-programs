3d6d23e60445d20998846fb7dbb1fdf25740faed
Dolesh kumar <hanubhai0007@gmail.com>
2026-08-04 14:44:57 +0530

    Add files via upload

---
+++ program10.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program10
+{
+    static void Main(string[] args)
+    {
+        int choice;
+
+        do
+        {
+            Console.WriteLine("\n===== MENU =====");
+            Console.WriteLine("1. Check Even/Odd");
+            Console.WriteLine("2. Find Largest of Three Numbers");
+            Console.WriteLine("3. Calculate Factorial");
+            Console.WriteLine("4. Exit");
+            Console.Write("Enter your choice: ");
+
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+
+                    Console.Write("Enter a number: ");
+                    int num = Convert.ToInt32(Console.ReadLine());
+
+                    if (num % 2 == 0)
+                        Console.WriteLine(num + " is Even.");
+                    else
+                        Console.WriteLine(num + " is Odd.");
+                    break;
+
+                case 2:
+      
+                    Console.Write("Enter first number: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter second number: ");
+                    int b = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter third number: ");
+                    int c = Convert.ToInt32(Console.ReadLine());
+
+                    int largest = a;
+
+                    if (b > largest)
+                        largest = b;
+
+                    if (c > largest)
+                        largest = c;
+
+                    Console.WriteLine("Largest Number = " + largest);
+                    break;
+
+                case 3:
+           
+                    Console.Write("Enter a number: ");
+                    int n = Convert.ToInt32(Console.ReadLine());
+
+                    long factorial = 1;
+
+                    for (int i = 1; i <= n; i++)
+                    {
+                        factorial *= i;
+                    }
+
+                    Console.WriteLine("Factorial = " + factorial);
+                    break;
+
+                case 4:
+                    Console.WriteLine("Exiting Program...");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Choice! Please try again.");
+                    break;
+            }
+
+        } while (choice != 4);
+    }
+}
