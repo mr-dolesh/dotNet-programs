1500dcdc20ccdfec2fa98c7854f638a625de4127
Dolesh kumar <hanubhai0007@gmail.com>
2026-08-04 14:35:35 +0530

    Add files via upload

---
+++ program6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class program6
+    {
+        public static string Odd { get; private set; }
+
+        static void Run(String[] args)
+        {
+            int N;
+            Console.WriteLine("Enter a number:");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < N; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("The given number is Even: " + i);
+                }
+                else
+                {
+                    Console.WriteLine("The given Number is Odd:" + Odd);
+                }
+            }
+        }
+    }
+}
