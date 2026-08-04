97be393d6a0486caaa9941360bab6a72a3d549e0
Dolesh kumar <hanubhai0007@gmail.com>
2026-08-04 13:54:49 +0530

    Add files via upload

---
+++ program04.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class program04
+    {
+        static void Main(String[] args)
+        {
+            int a;
+            Console.WriteLine("Enter the marks: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if(a <=100 && a >= 91)
+            {
+                Console.WriteLine("The grade is: O");
+            }
+            else if(a>=81 && a <= 90)
+            {
+                Console.WriteLine("The grade is: A+");
+            }
+            else if(a>=71 && a <= 80)
+            {
+                Console.WriteLine("The grade is: A");
+            }else if(a>=61 && a <= 70)
+            {
+                Console.WriteLine("The grade is: B+");
+            }else if(a>=51 && a <= 60)
+            {
+                Console.WriteLine("The grade is: B");
+            }
+            else
+            {
+                Console.WriteLine("The Student is fail in the exam.");
+            }
+        }
+    }
+}
